43b24d69ec4c82760ff36b301a71a6eb3d32f748
Ilya <kashak.work@gmail.com>
2018-04-30 12:01:21 +0000

    Added permissions to create, edit and remove business contacts

--- init.rb
+++ init.rb
@@ -5,7 +5,9 @@ Redmine::Plugin.register :business_contacts do
   version '0.0.1'
   url 'https://github.com/ilyabelyakov/business_contacts'
   author_url 'https://github.com/ilyabelyakov'
-  
+
+  permission :create_business_contacts, :business_contacts => [:new, :create, :create_manager]
+  permission :edit_business_contacts,   :business_contacts => [:edit, :update]
+  permission :remove_business_contacts, :business_contacts => [:destroy, :destroy_manager]
   menu :application_menu, :business_contacts, { :controller => 'business_contacts', :action => 'index' }, :caption => :label_business_contact_plural
-  # TODO: Add permissions
 end
